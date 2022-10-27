b664e77179c1884aff85ac37d7c998afd4821902
Hyosun Lee <hyosunlee@Hyosuns-MacBook-Pro.local>
2022-10-27 14:38:52 +0100

    test if the testing module is connected

--- src/movies/movies.controller.spec.ts
+++ src/movies/movies.controller.spec.ts
@@ -1,6 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../app.module';
+import * as request from 'supertest';
 
 const testingModule = Test.createTestingModule({
   imports: [AppModule],
@@ -12,4 +13,9 @@ beforeAll(async () => {
   const module = await testingModule.compile();
   app = await module.createNestApplication();
   await app.init();
+});
+
+it('should return empty list', async () => {
+  const response = await request(app.getHttpServer()).get('/movies').send();
+  expect(response.body).toEqual([]);
 });
